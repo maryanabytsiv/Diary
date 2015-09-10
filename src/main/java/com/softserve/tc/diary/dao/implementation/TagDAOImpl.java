916c286ad9bcbf28f7026aca0bf1a40352bc0de5
suprime2009 <apolopavel@gmail.com>
2015-09-10 23:20:54 +0300

    changes in TagDAO

--- src/main/java/com/softserve/tc/diary/dao/implementation/TagDAOImpl.java
+++ src/main/java/com/softserve/tc/diary/dao/implementation/TagDAOImpl.java
@@ -11,31 +11,28 @@ public class TagDAOImpl extends BaseDAOImpl<Tag> implements TagDAO {
 	@Override
 	public void create(Tag object) {
 		// TODO Auto-generated method stub
-		super.create(object);
 	}
 
 	@Override
 	public Tag readByKey(int id) {
 		// TODO Auto-generated method stub
-		return super.readByKey(id);
+		return null;
 	}
 
 	@Override
 	public void update(Tag object) {
 		// TODO Auto-generated method stub
-		super.update(object);
 	}
 
 	@Override
 	public void delete(Tag object) {
 		// TODO Auto-generated method stub
-		super.delete(object);
 	}
 
 	@Override
 	public List<Tag> getAll() {
 		// TODO Auto-generated method stub
-		return super.getAll();
+		return null;
 	}
 
 	public int getTagIdByTagMessage(String TagMessage) {
