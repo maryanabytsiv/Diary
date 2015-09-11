63f0296f3a01ae65cbfc2a2ae62d94c3a91001fd
suprime2009 <apolopavel@gmail.com>
2015-09-11 19:26:35 +0300

    add @after and @before in Tag

--- src/test/java/com/softserve/tc/diary/TestTagDAO.java
+++ src/test/java/com/softserve/tc/diary/TestTagDAO.java
@@ -11,7 +11,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -20,15 +22,11 @@ import com.softserve.tc.diary.entity.Tag;
 
 public class TestTagDAO {
 
-	// public static final String URL =
-	// "jdbc:postgresql://localhost:5432/DiaryTest";
-	// public static final String USER = "root";
-	// public static final String PASSWORD = "root";
 	private static Connection conn;
 	private static PreparedStatement ps;
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() throws SQLException {
 		conn = ConnectManager.getConnectionToTestDB();
 		BufferedReader br = null;
 		String scriptSQL;
@@ -55,36 +53,68 @@ public class TestTagDAO {
 		ps = conn.prepareStatement(result);
 		ps.execute();
 	}
-
+	
+	@Before
+	public void beforeTest() {
+		String baseValues = "insert into tag (uuid, tag_message) values('testkey1','#Hell');"+ '\n'
+							+ "insert into tag (uuid, tag_message) values('testkey2','#Hello');" + '\n'
+							+ "insert into tag (uuid, tag_message) values('testkey3','#HelloWorld');" + '\n'
+							+ "insert into tag (uuid, tag_message) values('testkey4','#HellGuy');" + '\n'
+							+ "insert into tag (uuid, tag_message) values('testkey5','#HellGuy');" + '\n'
+							+ "insert into tag (uuid, tag_message) values('testkey6','#HelpMe');";
+		try {
+			ps = conn.prepareStatement(baseValues);
+			ps.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@After
+	public void afterTest() {
+		String del = "delete from tag;";
+		try {
+			ps = conn.prepareStatement(del);
+			ps.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Test
 	public void testCreateTag() {
 		TagDAOImpl tagDAO = new TagDAOImpl();
-		Tag tag = new Tag("#HelloWorld");
-		tagDAO.create(tag);
-		tag = null;
+		Tag tagException = new Tag("#HelloWorld");
+		Tag workingTag = new Tag("#HelloWorldWide");
+		//tagDAO.create(tagException);
+		tagDAO.create(workingTag);
 		try {
-			ps = conn.prepareStatement("SELECT tag_message FROM tag " + 
-										"WHERE tag_message Like '#HelloWorld'");
+			String query1 = "SELECT tag_message FROM tag "
+								+ "WHERE tag_message Like '#HelloWorldWide';";
+//			String query2 = "SELECT tag_message FROM tag "
+//								+ "WHERE tag_message Like '#HelloWorld';";
+			ps = conn.prepareStatement(query1);
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
-				tag = new Tag(rs.getString("tag_message"));
+				workingTag = new Tag(rs.getString("tag_message"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
 		
-		assertEquals("#HelloWorld", tag.getTagMessage());
-
+		assertNotNull(workingTag);
+		assertEquals("#HelloWorldWide", workingTag.getTagMessage());
+		
+	//	assertEquals("#HelloWorld", tagException.getTagMessage());
 	}
 
 	@Test
-	public void testReadByKey() {
-
+	public void testGetListTagsByPrefix() {
 		TagDAOImpl tagDAO = new TagDAOImpl();
-
-		// Tag tag = tagDAO.readByKey(key);
-
+		String prefix = "#Hello";
+		
+		
+		
 	}
 
 }
