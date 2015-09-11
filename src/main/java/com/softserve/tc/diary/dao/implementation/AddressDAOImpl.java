02e6a4078a957c075750a62227ac1bf149767562
Bug5Bunny <mural@ex.ua>
2015-09-11 11:29:43 +0300

    add method create Address

--- src/main/java/com/softserve/tc/diary/dao/implementation/AddressDAOImpl.java
+++ src/main/java/com/softserve/tc/diary/dao/implementation/AddressDAOImpl.java
@@ -1,5 +1,9 @@
 package com.softserve.tc.diary.dao.implementation;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.softserve.tc.diary.dao.AddressDAO;
@@ -8,68 +12,98 @@ import com.softserve.tc.diary.entity.User;
 
 public class AddressDAOImpl extends BaseDAOImpl<Address>implements AddressDAO {
 
-	public void create(Address object) {
-		// TODO Auto-generated method stub
-		
-	}
+    public static final String URL = "jdbc:postgresql://localhost:5432/DiaryTest";
+    public static final String USER = "root";
+    public static final String PASSWORD = "root";
+    private static Connection conn = null;
+    private static PreparedStatement ps;
 
-	public Address readByKey(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private static void getConnection() {
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void update(Address object) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void create(Address object) {
 
-	public void delete(Address object) {
-		// TODO Auto-generated method stub
-		
-	}
+        getConnection();
+        try {
+            ps = conn.prepareStatement("insert into address values(?,?,?,?,?)");
+            ps.setInt(1, object.getId());
+            ps.setString(2, object.getCountry());
+            ps.setString(2, object.getCity());
+            ps.setString(2, object.getStreet());
+            ps.setInt(5,object.getBuild_number());
+            ps.execute();
+            ps.close();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
-	public List<User> getUsersByCountry(String country) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Address readByKey(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	public List<User> getUsersByCity(String city) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public void update(Address object) {
+        // TODO Auto-generated method stub
 
-	public List<Address> getByCity(String city) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	public String getMostCommonCountry() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public void delete(Address object) {
+        // TODO Auto-generated method stub
 
-	public int countAllByCountry(String country) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    }
 
-	public int countAllByCity(String city) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public List<User> getUsersByCountry(String country) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	public int countAllByStreet(String city) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public List<User> getUsersByCity(String city) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	public List<Address> getAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public List<Address> getByCity(String city) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	
+    public String getMostCommonCountry() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
+    public int countAllByCountry(String country) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
+    public int countAllByCity(String city) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public int countAllByStreet(String city) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public List<Address> getAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
