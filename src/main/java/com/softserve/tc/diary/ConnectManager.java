c9ddaa926a5567d82317287f8e8ab39b20ba9196
andriishupta <andrew260794@gmail.com>
2015-09-11 16:18:20 +0300

    Connection and pom.xml

--- src/main/java/com/softserve/tc/diary/ConnectManager.java
+++ src/main/java/com/softserve/tc/diary/ConnectManager.java
@@ -1,29 +1,48 @@
-//package com.softserve.tc.diary;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-//
-//import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
-//
-///**
-// * @author Andrii Shupta
-// *
-// */
-//public final class ConnectManager {
-//	public static final BasicDataSource dataSource = new BasicDataSource();
-//	static{
-//		dataSource.setDriverClassName("com.postgres.Driver");
-//		dataSource.setUrl("jdbc:postgresql://localhost:5432/diary");
-//        dataSource.setUsername("postgres");
-//        dataSource.setPassword("password"); 
-//	}
-//	private ConnectManager(){
-//		//
-//	}
-//	
-//	public static Connection getConnection() throws SQLException{
-//       return dataSource.getConnection();
-//    }
-//}
-//
+package com.softserve.tc.diary;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+/**
+ * @author Andrii Shupta
+ *
+ */
+public final class ConnectManager {
+	public static final BasicDataSource dataSource = new BasicDataSource();
+	public static final BasicDataSource dataSourceTestDB = new BasicDataSource();
+
+	static {
+		dataSource.setDriverClassName("com.postgres.Driver");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/diary");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+
+		dataSourceTestDB.setDriverClassName("com.postgres.Driver");
+		dataSourceTestDB.setUrl("jdbc:postgresql://localhost:5432/diarytest");
+		dataSourceTestDB.setUsername("root");
+		dataSourceTestDB.setPassword("root");
+	}
+
+	private ConnectManager() {
+		//
+	}
+
+	public static Connection getConnectionToTestDB() throws SQLException {
+		return dataSourceTestDB.getConnection();
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return dataSource.getConnection();
+	}
+	
+	public static void closeConnectionToTestDB() throws SQLException{
+		dataSourceTestDB.close();
+	}
+
+	public static void closeConnection() throws SQLException{
+		dataSource.close();
+	}
+	
+}
