309064389f74dbfa6b2254a48438414db0424f85
Teodor Mysko <teodor.mysko@gmail.com>
2015-09-15 01:10:09 +0300

    Complited DAO and tests for User entity

--- src/test/java/com/softserve/tc/diary/TestUserDAO.java
+++ src/test/java/com/softserve/tc/diary/TestUserDAO.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertNull;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -23,27 +24,38 @@ public class TestUserDAO {
 	public static void setUpBeforeClass() throws SQLException {
 		Query.setUpBeforeClass();
 	}
+
 	@AfterClass
 	public static void tearDownAfterClass() throws SQLException {
 		Query.DropTableIfExists();
+		UserDAOImpl.close();
 	}
+
 	@Before
-	public void beforeTest() throws SQLException{
+	public void beforeTest() throws SQLException {
 		Query.insertValue();
 	}
+
 	@After
-	public void afterTest() throws SQLException{
+	public void afterTest() throws SQLException {
 		Query.deleteAllFromTable();
+
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testCreateUserNullPointerException() {
+		UserDAOImpl userDAO = new UserDAOImpl();
+		userDAO.create(new User(null, "Andriy", "Mural", "1", null, "64561", Sex.FEMALE, "1999-03-02",
+				"folder/folder/image.png", null));
 	}
 
 	@Test
 	public void testCreateUser() {
 		UserDAOImpl userDAO = new UserDAOImpl();
-		userDAO.create(
-				new User("hary12", "Andriy", "Mural", "1", "bg@gmail.com", "64561", Sex.FEMALE, "1995-03-02", "folder/folder/image.png", "2"));
+		userDAO.create(new User("hary12", "Andriy", "Mural", "1", "bg@gmail.com", "64561", Sex.FEMALE, "1995-03-02",
+				"folder/folder/image.png", "2"));
 		User userActual = new User();
 		try {
-
 			Query.ps = Query.connection.prepareStatement("select * from user_card where nick_name ='hary12';");
 			ResultSet rs = Query.ps.executeQuery();
 			while (rs.next()) {
@@ -61,15 +73,13 @@ public class TestUserDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
 		assertNotNull(userActual);
 		assertEquals("hary12", userActual.getNick_name());
 		assertEquals("Andriy", userActual.getFirst_name());
 		assertEquals("Mural", userActual.getSecond_name());
 		assertEquals("bg@gmail.com", userActual.getE_mail());
 		assertEquals("1", userActual.getAddress());
-		//assertEquals("64561", userActual.getPassword());
-	    assertEquals("FEMALE", userActual.getSex());
+		assertEquals("FEMALE", userActual.getSex());
 		assertEquals("1995-03-02", userActual.getDate_of_birth());
 		assertEquals("folder/folder/image.png", userActual.getAvatar());
 		assertEquals("2", userActual.getRole());
@@ -77,38 +87,37 @@ public class TestUserDAO {
 
 	@Test
 	public void testUpdateUser() {
-        	 
 		UserDAOImpl userDAO = new UserDAOImpl();
-        
-		User user = new User("read", "Natalya", "Bolyk", "1", "bg@gmail.com", "64561", Sex.FEMALE, "1999-10-10", "some.jpeg",
-		"1");
+		User user = new User("read", "Natalya", "Bolyk", "1", "bg@gmail.com", "64561", Sex.FEMALE, "1999-10-10",
+				"some.jpeg", "1");
 		user.setUuid(userDAO.getGeneratedId());
-        try {
-        	Query.ps = Query.connection.prepareStatement("insert into user_card values(?,?,?,?,?,?,?,?,CAST(? AS DATE),?,?);");
-        	Query.ps.setString(1, user.getUuid());
-        	Query.ps.setString(2, user.getNick_name());
-        	Query.ps.setString(3, user.getFirst_name());
-        	Query.ps.setString(4, user.getSecond_name());
-        	Query.ps.setString(5, user.getAddress());
-        	Query.ps.setString(6, user.getE_mail());
-        	Query.ps.setString(7, user.getPassword());
-        	Query.ps.setString(8, user.getSex());
-        	Query.ps.setString(9, user.getDate_of_birth());
-        	Query.ps.setString(10, user.getAvatar());
-        	Query.ps.setString(11, user.getRole());
-        	Query.ps.execute();
-        } catch (SQLException e1) {
-            e1.printStackTrace();
-        }
-        user.setFirst_name("IRA");
-        user.setSecond_name("BLLLLL");
-        userDAO.update(user);
-        User userActual = new User();
-        try {
-        	Query.ps = Query.connection.prepareStatement("select * from user_card where nick_name =?");
-        	Query.ps.setString(1, user.getNick_name());
-            ResultSet rs = Query.ps.executeQuery();
-            while (rs.next()) {
+		try {
+			Query.ps = Query.connection
+					.prepareStatement("insert into user_card values(?,?,?,?,?,?,?,?,CAST(? AS DATE),?,?);");
+			Query.ps.setString(1, user.getUuid());
+			Query.ps.setString(2, user.getNick_name());
+			Query.ps.setString(3, user.getFirst_name());
+			Query.ps.setString(4, user.getSecond_name());
+			Query.ps.setString(5, user.getAddress());
+			Query.ps.setString(6, user.getE_mail());
+			Query.ps.setString(7, user.getPassword());
+			Query.ps.setString(8, user.getSex());
+			Query.ps.setString(9, user.getDate_of_birth());
+			Query.ps.setString(10, user.getAvatar());
+			Query.ps.setString(11, user.getRole());
+			Query.ps.execute();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		user.setFirst_name("IRA");
+		user.setSecond_name("BLLLLL");
+		userDAO.update(user);
+		User userActual = new User();
+		try {
+			Query.ps = Query.connection.prepareStatement("select * from user_card where nick_name =?");
+			Query.ps.setString(1, user.getNick_name());
+			ResultSet rs = Query.ps.executeQuery();
+			while (rs.next()) {
 				userActual.setNick_name(rs.getString("nick_name"));
 				userActual.setFirst_name(rs.getString("first_name"));
 				userActual.setSecond_name(rs.getString("second_name"));
@@ -119,11 +128,11 @@ public class TestUserDAO {
 				userActual.setDate_of_birth(rs.getString("date_of_birth"));
 				userActual.setAvatar(rs.getString("avatar"));
 				userActual.setRole(rs.getString("role"));
-            }
+			}
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		assertNotNull(userActual);
 		assertEquals("read", userActual.getNick_name());
 		assertEquals("IRA", userActual.getFirst_name());
@@ -131,23 +140,20 @@ public class TestUserDAO {
 		assertEquals("bg@gmail.com", userActual.getE_mail());
 		assertEquals("1", userActual.getAddress());
 		assertEquals("64561", userActual.getPassword());
-	    assertEquals("FEMALE", userActual.getSex());
+		assertEquals("FEMALE", userActual.getSex());
 		assertEquals("1999-10-10", userActual.getDate_of_birth());
 		assertEquals("some.jpeg", userActual.getAvatar());
 		assertEquals("1", userActual.getRole());
-
 	}
 
 	@Test
 	public void testDeleteUser() {
-		
 		UserDAOImpl userDAO = new UserDAOImpl();
 		User user = new User("delete", "Natalya", "Bolyk", "1", "bg@gmail.com", "64561", Sex.FEMALE, null, "jfhfff.mvn",
 				"1");
 		userDAO.create(user);
 		userDAO.delete(user);
 		assertNull(userDAO.readByKey("delete"));
-
 	}
 
 	@Test
@@ -156,55 +162,61 @@ public class TestUserDAO {
 		User user = new User("Bozo", "Oleg", "Ponkin", "2", "bsss@gmail.com", "64561", Sex.MALE, null, "jsjwe.txt",
 				"1");
 		userDAO.create(user);
-        int actual = userDAO.getAll().size();
-        int expected = 4;
-        assertEquals(expected, actual);
+		int actual = userDAO.getAll().size();
+		int expected = 4;
+		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void testGetByNickName() {
-		 User userActual = new User();
-	        try {
-	        	Query.ps = Query.connection.prepareStatement("select * from user_card where nick_name =?");
-	        	Query.ps.setString(1, "TreeTree");
-	            ResultSet rs = Query.ps.executeQuery();
-	            while (rs.next()) {
-					userActual.setNick_name(rs.getString("nick_name"));
-					userActual.setFirst_name(rs.getString("first_name"));
-					userActual.setSecond_name(rs.getString("second_name"));
-					userActual.setAddress(rs.getString("address_id"));
-					userActual.setE_mail(rs.getString("e_mail"));
-					userActual.setPassword(rs.getString("password"));
-					userActual.setSex(rs.getString("Sex"));
-					userActual.setDate_of_birth(rs.getString("date_of_birth"));
-					userActual.setAvatar(rs.getString("avatar"));
-					userActual.setRole(rs.getString("role"));
-	            }
+		User userActual = new User();
+		try {
+			Query.ps = Query.connection.prepareStatement("select * from user_card where nick_name =?");
+			Query.ps.setString(1, "TreeTree");
+			ResultSet rs = Query.ps.executeQuery();
+			while (rs.next()) {
+				userActual.setNick_name(rs.getString("nick_name"));
+				userActual.setFirst_name(rs.getString("first_name"));
+				userActual.setSecond_name(rs.getString("second_name"));
+				userActual.setAddress(rs.getString("address_id"));
+				userActual.setE_mail(rs.getString("e_mail"));
+				userActual.setPassword(rs.getString("password"));
+				userActual.setSex(rs.getString("Sex"));
+				userActual.setDate_of_birth(rs.getString("date_of_birth"));
+				userActual.setAvatar(rs.getString("avatar"));
+				userActual.setRole(rs.getString("role"));
+			}
 
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        }
-			assertNotNull(userActual);
-			assertEquals("TreeTree", userActual.getNick_name());
-			assertEquals("Sergey", userActual.getFirst_name());
-			assertEquals("Gontar", userActual.getSecond_name());
-			assertEquals("jhfcjfdf@gmail.com", userActual.getE_mail());
-			assertEquals("3", userActual.getAddress());
-			assertEquals("flgkjhlkftjt", userActual.getPassword());
-		    assertEquals("MALE", userActual.getSex());
-			assertEquals("1989-02-20", userActual.getDate_of_birth());
-			assertEquals(null, userActual.getAvatar());
-			assertEquals("2", userActual.getRole());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		assertNotNull(userActual);
+		assertEquals("TreeTree", userActual.getNick_name());
+		assertEquals("Sergey", userActual.getFirst_name());
+		assertEquals("Gontar", userActual.getSecond_name());
+		assertEquals("jhfcjfdf@gmail.com", userActual.getE_mail());
+		assertEquals("3", userActual.getAddress());
+		assertEquals("flgkjhlkftjt", userActual.getPassword());
+		assertEquals("MALE", userActual.getSex());
+		assertEquals("1989-02-20", userActual.getDate_of_birth());
+		assertEquals(null, userActual.getAvatar());
+		assertEquals("2", userActual.getRole());
 	}
 
 	@Test
 	public void testCountAllBySex() {
-
+		UserDAOImpl user = new UserDAOImpl();
+		int count = user.countAllBySex("MALE");
+		assertEquals(2, count);
 	}
 
 	@Test
 	public void testGetByDateOfBirth() {
-
+		UserDAOImpl user = new UserDAOImpl();
+		List<User> list = user.getByYearOfBirth("1989");
+		int actual = list.size();
+		int expected = 1;
+		assertEquals(expected, actual);
 
 	}
 
