b363562a2415777b6d00a848d2e8fc3c5201b731
Thomas Shelby <mikola1693@gmail.com>
2015-09-14 15:32:44 +0300

    fixed TestAddressDAO to new class Query

--- src/test/java/com/softserve/tc/diary/TestAddressDAO.java
+++ src/test/java/com/softserve/tc/diary/TestAddressDAO.java
@@ -4,13 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -20,50 +16,15 @@ import com.softserve.tc.diary.entity.Address;
 
 public class TestAddressDAO {
 
-    private static Connection conn;
-    private static PreparedStatement ps;
 
     @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-
-        BufferedReader br = null;
-        String scriptSQL;
-        String result = "";
-        try {
-            br = new BufferedReader(new FileReader("./PostgreSQL_DB/DiaryTest.sql"));
-            while ((scriptSQL = br.readLine()) != null) {
-                result += scriptSQL + "\n";
-
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (br != null)
-                    br.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-
-        try {
-            conn = ConnectManager.getConnectionToTestDB();
-            ps = conn.prepareStatement(result);
-            ps.execute();
-            ps.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void setUpBeforeClass() throws SQLException {
+    	Query.setUpBeforeClass();
     }
 
     @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-        ps = conn.prepareStatement("DROP TABLE IF EXISTS tag_record;" + "DROP TABLE IF EXISTS record_list;"
-                + "DROP TABLE IF EXISTS user_card;" + "DROP TABLE IF EXISTS address;" + "DROP TABLE IF EXISTS role;"
-                + "DROP TABLE IF EXISTS tag;");
-        ps.execute();
-        ps.close();
+    public static void tearDownAfterClass() throws SQLException {
+    	Query.DropTableIfExists();
     }
 
     @Test
@@ -75,18 +36,17 @@ public class TestAddressDAO {
         Address address = null;
 
         try {
-            ps = conn.prepareStatement(
+            Query.ps = Query.connection.prepareStatement(
                     "select * from address where country = ? and city = ? and street = ? and build_number = ?");
-            ps.setString(1, newAddress.getCountry());
-            ps.setString(2, newAddress.getCity());
-            ps.setString(3, newAddress.getStreet());
-            ps.setInt(4, newAddress.getBuild_number());
-            ResultSet rs = ps.executeQuery();
+            Query.ps.setString(1, newAddress.getCountry());
+            Query.ps.setString(2, newAddress.getCity());
+            Query.ps.setString(3, newAddress.getStreet());
+            Query.ps.setInt(4, newAddress.getBuild_number());
+            ResultSet rs = Query.ps.executeQuery();
             while (rs.next()) {
                 address = new Address(rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5));
             }
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
@@ -106,30 +66,28 @@ public class TestAddressDAO {
         Address add = new Address("jhbkjhbkj", "fdfsfy", "Nsfsfft", 16);
         add.setId(addressDAO.getGeneratedId());
         try {
-            ps = conn.prepareStatement("insert into address(id, country, city, street, build_number) values(?,?,?,?,?)");
-            ps.setString(1, add.getId());
-            ps.setString(2, add.getCountry());
-            ps.setString(3, add.getCity());
-            ps.setString(4, add.getStreet());
-            ps.setInt(5, add.getBuild_number());
-            ps.execute();
+        	Query.ps = Query.connection.prepareStatement("insert into address(id, country, city, street, build_number) values(?,?,?,?,?)");
+        	Query.ps.setString(1, add.getId());
+        	Query.ps.setString(2, add.getCountry());
+        	Query.ps.setString(3, add.getCity());
+        	Query.ps.setString(4, add.getStreet());
+        	Query.ps.setInt(5, add.getBuild_number());
+        	Query.ps.execute();
         } catch (SQLException e1) {
-            // TODO Auto-generated catch block
             e1.printStackTrace();
         }
         addressDAO.update(add);
         Address address = null;
         try {
-            ps = conn.prepareStatement("select * from address where id=?");
-            ps.setString(1, add.getId());
-            ResultSet rs = ps.executeQuery();
+        	Query.ps = Query.connection.prepareStatement("select * from address where id=?");
+        	Query.ps.setString(1, add.getId());
+            ResultSet rs = Query.ps.executeQuery();
             while (rs.next()) {
                 address = new Address(rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5));
                 address.setId(rs.getString(1));
             }
 
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         assertEquals(add.getCountry(), address.getCountry());
