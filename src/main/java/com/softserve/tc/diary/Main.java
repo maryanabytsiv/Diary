e6e97a736eb1f44fd8d055b65f35e0b44db0f71a
Maryana Bytsiv <Maryana Bytsiv>
2015-09-15 10:52:06 +0300

    web service interface, password encrypt fixed

--- src/main/java/com/softserve/tc/diary/Main.java
+++ src/main/java/com/softserve/tc/diary/Main.java
@@ -1,16 +1,8 @@
 package com.softserve.tc.diary;
 
-import com.softserve.tc.diary.dao.implementation.UserDAOImpl;
-import com.softserve.tc.diary.entity.Sex;
-import com.softserve.tc.diary.entity.User;
-
 public class Main {
 
 	public static void main(String[] args) {
-		UserDAOImpl user=new UserDAOImpl();
-		User newUser=new User("Nikko", "Thomas", "Shelby", "1", "mtungieugn@gmail", "sefhwufh", Sex.FEMALE, "1991-12-12", "frwnfef", "1");
-		user.create(newUser);
 
 	}
-
 }
