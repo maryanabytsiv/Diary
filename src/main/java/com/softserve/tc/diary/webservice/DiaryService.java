76794bb4b01e58253a560e5933451e9b2e2991aa
suprime2009 <apolopavel@gmail.com>
2015-09-18 15:25:14 +0300

    Web Service

--- src/main/java/com/softserve/tc/diary/webservice/DiaryService.java
+++ src/main/java/com/softserve/tc/diary/webservice/DiaryService.java
@@ -12,19 +12,21 @@ import com.softserve.tc.diary.entity.Status;
 
 @WebService
 public interface DiaryService {
+	
+	 boolean logIn(String nickName, String password);
     
-    boolean logIn(String nickName, String password);
-    
-    boolean logOut(String nickName);
-    
-    boolean addRecord(String nickname, Status status, String record);
-    
-    boolean removeRecord(String nickname, String record);
-    
-    List<Record> getAllRecords(String nickName, Date date);
-    
-    List<Record> getAllRecords(String nickName, String hashTag);
-    
-    Statistics viewSiteStatistics(String nickNameOfAdmin);
+//    boolean logIn(String nickName, String password);
+//    
+//    boolean logOut(String nickName);
+//    
+//    boolean addRecord(String nickname, Status status, String record);
+//    
+//    boolean removeRecord(String nickname, String record);
+//    
+//    List<Record> getAllRecords(String nickName, Date date);
+//    
+//    List<Record> getAllRecords(String nickName, String hashTag);
+//    
+//    Statistics viewSiteStatistics(String nickNameOfAdmin);
     
 }
