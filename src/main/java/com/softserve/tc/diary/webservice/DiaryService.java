e7458ff26ea6f05eec9a2b0e8472aa39562a16c2
AndriyZhmurkevych <andriyzhmurkevych@gmail.com>
2015-09-20 23:59:39 +0300

    added addRecord Implementation for DiaryService

--- src/main/java/com/softserve/tc/diary/webservice/DiaryService.java
+++ src/main/java/com/softserve/tc/diary/webservice/DiaryService.java
@@ -2,17 +2,19 @@ package com.softserve.tc.diary.webservice;
 
 import javax.jws.WebService;
 
+import com.softserve.tc.diary.entity.Status;
+
 @WebService
 public interface DiaryService {
     
     String logIn(String nickName, String password);
     
     boolean logOut(String nickName);
-    //
-    // boolean addRecord(String nickname, Status status, String record);
-    //
-    // boolean removeRecord(String nickname, String record);
-    //
+    
+    boolean addRecord(String nickname, Status status, String record);
+    
+    boolean removeRecord(String nickname, String record);
+    
     // List<Record> getAllRecords(String nickName, Date date);
     //
     // List<Record> getAllRecords(String nickName, String hashTag);
--- src/main/java/com/softserve/tc/diary/webservice/DiaryServiceImpl.java
+++ src/main/java/com/softserve/tc/diary/webservice/DiaryServiceImpl.java
@@ -1,19 +1,27 @@
 package com.softserve.tc.diary.webservice;
 
+import java.sql.Timestamp;
 import java.util.UUID;
 
 import javax.jws.WebService;
 
 import org.apache.log4j.Logger;
 
+import com.softserve.tc.diary.dao.RecordDAO;
+import com.softserve.tc.diary.dao.UserDAO;
+import com.softserve.tc.diary.dao.implementation.RecordDAOImpl;
 import com.softserve.tc.diary.dao.implementation.UserDAOImpl;
+import com.softserve.tc.diary.entity.Record;
+import com.softserve.tc.diary.entity.Status;
 import com.softserve.tc.diary.entity.User;
 import com.softserve.tc.log.Log;
 
 @WebService
 public class DiaryServiceImpl implements DiaryService {
     private static Logger LOG = Log.init("DiaryServiceImpl");
-    private UserDAOImpl userDAO = new UserDAOImpl();
+    
+    private UserDAO userDAO = new UserDAOImpl();
+    private RecordDAO recordDAOImpl = new RecordDAOImpl();
     
     public String logIn(String nickName, String password) {
         User user = userDAO.findByNickName(nickName);
@@ -30,7 +38,7 @@ public class DiaryServiceImpl implements DiaryService {
                 return session;
             }
             LOG.debug(
-                    String.format("Icorrect password of user with nickname %s",
+                    String.format("Incorrect password of user with nickname %s",
                             nickName));
                             
             return null;
@@ -49,19 +57,33 @@ public class DiaryServiceImpl implements DiaryService {
         
         return true;
     }
-    //
-    // @Override
-    // public boolean addRecord(String nickname, Status status, String record) {
-    // // TODO Auto-generated method stub
-    // return false;
-    // }
-    //
-    // @Override
-    // public boolean removeRecord(String nickname, String record) {
-    // // TODO Auto-generated method stub
-    // return false;
-    // }
-    //
+    
+    @Override
+    public boolean addRecord(String nickname, Status status, String record) {
+        User user = userDAO.findByNickName(nickname);
+        if (user == null) {
+            LOG.debug(String.format("User was not found by nickname %s",
+                    nickname));
+                    
+            return false;
+        } else {
+            Timestamp createdTime =
+                    new Timestamp(new java.util.Date().getTime());
+                    
+            Record newRecord = new Record(user.getUuid(), createdTime, record,
+                    null, status);
+            recordDAOImpl.create(newRecord);
+            
+            return true;
+        }
+    }
+    
+    @Override
+    public boolean removeRecord(String nickname, String record) {
+        // TODO Ask about Record. and nickname(maybe there must be session?)
+        return false;
+    }
+    
     // @Override
     // public List<Record> getAllRecords(String nickName, Date date) {
     // // TODO Auto-generated method stub
