263d8b2d5f4f229b12040d2b540853481149938e
andriishupta <andrew260794@gmail.com>
2015-10-04 16:09:21 +0300

    changed timestamp adapter

--- src/main/java/com/softserve/tc/diary/adapter/TimestampAdapter.java
+++ src/main/java/com/softserve/tc/diary/adapter/TimestampAdapter.java
@@ -1,6 +1,7 @@
 package com.softserve.tc.diary.adapter;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
@@ -14,7 +15,9 @@ public class TimestampAdapter extends XmlAdapter<String, Timestamp>{
 
     @Override
     public Timestamp unmarshal(String v) throws Exception {
-        return new Timestamp(Long.parseLong(v));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date parsedDate = dateFormat.parse(v);
+        return new Timestamp(parsedDate.getTime());
     }
 
 }
